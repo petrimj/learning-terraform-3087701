3eee58942a8b08342faebf6ee08e7af22bf9e880
Petri Mäki-Jaakkola <petri@frans.fi>
2024-02-09 09:26:57 +0200

    Fix 2

--- main.tf
+++ main.tf
@@ -39,8 +39,8 @@ module "blog_sg" {
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  eggress_rules       = ["all-all"]
-  eggress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
 
